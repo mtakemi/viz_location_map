1b0eaea4641f82fe04634333cdb76c9c66358401
m.takemi <m.takemi@gmail.com>
2022-11-28 21:41:00 +0900

    add zoom option, etc.

--- appserver/static/visualizations/location_map/src/visualization_source.js
+++ appserver/static/visualizations/location_map/src/visualization_source.js
@@ -24,8 +24,10 @@ function(
   
   // Extend from SplunkVisualizationBase
   return SplunkVisualizationBase.extend({
-
+    isInitialized: false,
     map: null,
+    markers: [],
+    polylines: [],
 
     initialize: function() {
       SplunkVisualizationBase.prototype.initialize.apply(this, arguments);
@@ -45,31 +47,35 @@ function(
       return data;
     },
 
-    initLoad: function(tileUrl, tileAttr, kmlUrl){
+    initLoad: function(tileUrl, tileAttr, kmlUrl, maxZoom, minZoom, initialZoom){
       if(this.map) return;
 
-      this.map = L.map('map').setView([35.65809922, 139.74135747], 8);
-      let mapLink = '<a href="https://openstreetmap.org">OpenStreetMap</a>';
+      maxZoom = maxZoom || 18;
+      minZoom = minZoom || 0;
+      initialZoom = initialZoom || 15;
+
+      this.map = L.map('map')
       L.tileLayer(
         tileUrl, {
           attribution: tileAttr,
-          maxZoom: 18
+          maxZoom: maxZoom,
+          minZoom: minZoom,
         }).addTo(this.map);
+      this.map.setZoom(initialZoom)
 
       let self = this
       //var kml = '/en-US/static/app/viz_location_map/tokyo.kml';
-
-      fetch(kmlUrl)
-	.then(res => res.text())
-	.then(kmltext => {
-	  const parser = new DOMParser();
-	  const kml = parser.parseFromString(kmltext, 'text/xml');
-	  const track = new L.KML(kml);
-	  self.map.addLayer(track);
-	  const bounds = track.getBounds();
-	  self.map.fitBounds(bounds);
-	});
-
+      if(kmlUrl){
+        fetch(kmlUrl)
+	        .then(res => res.text())
+	        .then(kmltext => {
+	          const parser = new DOMParser();
+	          const kml = parser.parseFromString(kmltext, 'text/xml');
+	          const track = new L.KML(kml);
+	          self.map.addLayer(track);
+	        });
+      }
+      self.isInitialized = true
     },
 
     _getProperty(config, key, def){
@@ -85,69 +91,85 @@ function(
 
       if(!rows.length) return
 
-      // Draw something here
-      //console.log(config)
-      //console.log(data)
-      //console.log(L)
-
       let tileUrl = this._getProperty(config, 'tileLayerUrl')
       let tileAttribution = this._getProperty(config, 'tileLayerAttribution')
       let overlayKmlUrl = this._getProperty(config, 'overlayKmlUrl')
+      let maxZoom = this._getProperty(config, 'maxZoom')
+      let minZoom = this._getProperty(config, 'minZoom')
+      let initialZoom = this._getProperty(config, 'initialZoom')
+      let moveLastLocation = this._getProperty(config, 'moveLastLocation') == "yes"
 
+      this.initLoad(tileUrl, tileAttribution, overlayKmlUrl,
+                    maxZoom, minZoom, initialZoom);
 
-      this.initLoad(tileUrl, tileAttribution, overlayKmlUrl);
-
-      let obj = {}
+      let obj = {};
+      let lastLocation = null;
       rows.forEach(function(v){
-      //for(let n1 = 0; n1 < rows.length; ++n1){
-        // _time, lat, lng, name, icon, color
-        console.log(v)
-	        let _time = v[0];
-	        let latlng = new L.LatLng(parseFloat(v[1]), parseFloat(v[2]), 0);
-	        let name = v[3];
-	        let icon = v[4];
-	        let color = v[5];
-	        if(obj.hasOwnProperty(name)){
-	            obj[name].push({
-	                'time': _time,
-	                'latlng': latlng,
-	                'icon': icon,
-	                'color': color
-	            })
-	        } else {
-	            obj[name] = [{
-	                'time': _time,
-	                'latlng': latlng,
-	                'icon': icon,
-	                'color': color
-	            }]
-	        }
-      })
-      //console.log(obj)
+	      let _time = v[0];
+	      let latlng = new L.LatLng(parseFloat(v[1]), parseFloat(v[2]), 0);
+	      let name = v[3];
+	      let icon = v[4];
+	      let color = v[5];
+	      if(obj.hasOwnProperty(name)){
+	        obj[name].push({
+	          'time': _time,
+	          'latlng': latlng,
+	          'icon': icon,
+	          'color': color
+	        });
+	      } else {
+	        obj[name] = [{
+	          'time': _time,
+	          'latlng': latlng,
+	          'icon': icon,
+	          'color': color
+	        }];
+	      }
+      });
 
-      let self = this
+      let self = this;
+
+      self.markers.forEach(function(v){
+        self.map.removeLayer(v)
+      });
+      self.markers = []
+
+      self.polylines.forEach(function(v){
+        self.map.removeLayer(v)
+      });
+      self.polylines = [];
 
       Object.keys(obj).forEach(function(key){
       //for(let key of Object.keys(obj)){
-        let c = _.pluck(obj[key], 'latlng')
-        let cl = obj[key][0]['color']
-        let ic = obj[key][0]['icon']
+        let c = _.pluck(obj[key], 'latlng');
+        let cl = obj[key][0]['color'];
+        let ic = obj[key][0]['icon'];
         let opt = {
           'color': cl
+        };
+        if(self.polyline){
+          self.map.removeLayer(self.polyline);
         }
-        let pl = new L.Polyline(c, opt);
-        pl.addTo(self.map)
+        polyline = new L.Polyline(c, opt);
+        polyline.addTo(self.map);
+        self.polylines.push(polyline);
 
         let icopt = {
           'markerColor': cl,
           'icon': ic
-        }
-        //let vmk = LeafletVM.icon(icopt)
-        let vmk = LeafletVM.VectorMarkers.icon(icopt)
-        let mk = new L.marker(c.slice(-1)[0], {'icon': vmk})
-        mk.addTo(self.map)
-        
-      })
+        };
+
+        //let vmarker = LeafletVM.icon(icopt)
+        let vmarker = LeafletVM.VectorMarkers.icon(icopt);
+        lastLocation = c.slice(-1)[0];
+        marker = new L.marker(lastLocation, {'icon': vmarker});
+        marker.addTo(self.map);
+        self.markers.push(marker);
+      });
+
+      if(moveLastLocation){
+        self.map.panTo(lastLocation);
+      }
     },
 
     // Search data params
